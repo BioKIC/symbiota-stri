d20732bffbcd5deacbcda75828b4771f0ea1c7ce
Edward Gilbert <egbot@asu.edu>
2021-09-13 16:13:35 -0700

    Force update of stylesheets

--- includes/head.php
+++ includes/head.php
@@ -13,5 +13,5 @@ if($activateJQuery){
 	echo '<link href="'.$CLIENT_ROOT.'/css/jquery-ui.css" type="text/css" rel="stylesheet">';
 }
 ?>
-<link href="<?php echo $CLIENT_ROOT; ?>/css/base.css?ver=1" type="text/css" rel="stylesheet">
-<link href="<?php echo $CLIENT_ROOT; ?>/css/main.css?ver=1" type="text/css" rel="stylesheet">
+<link href="<?php echo $CLIENT_ROOT; ?>/css/base.css?ver=1" type="text/css" rel="stylesheet" />
+<link href="<?php echo $CLIENT_ROOT; ?>/css/main.css?ver=2" type="text/css" rel="stylesheet" />
