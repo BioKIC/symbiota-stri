cc9d6689709ce2dfb4372aa866ee23490c2e9903
Edward Gilbert <egbot@asu.edu>
2021-09-14 13:02:54 -0700

    Header style adjustments

--- includes/head.php
+++ includes/head.php
@@ -9,4 +9,4 @@ $cssPathPrefix = $CLIENT_ROOT.'/css/symb';
 <meta name="viewport" content="initial-scale=1.0, user-scalable=yes" />
 <link href="<?php echo $CLIENT_ROOT; ?>/css/jquery-ui.css" type="text/css" rel="stylesheet" />
 <link href="<?php echo $CLIENT_ROOT; ?>/css/base.css?ver=1" type="text/css" rel="stylesheet" />
-<link href="<?php echo $CLIENT_ROOT; ?>/css/main.css?ver=3" type="text/css" rel="stylesheet" />
+<link href="<?php echo $CLIENT_ROOT; ?>/css/main.css?ver=4" type="text/css" rel="stylesheet" />
